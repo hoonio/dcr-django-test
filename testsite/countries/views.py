7f989d87e82762fc0b601a5abe308dd0e0ac5d26
Hoonio <hoonio@hoonio.com>
2023-03-28 09:07:08 +0100

    Exercise 1

--- testsite/countries/views.py
+++ testsite/countries/views.py
@@ -1,8 +1,28 @@
 from django.http import JsonResponse
+import json
+from .models import Country, Region
+from functools import reduce
 
+
+def init_regions(region):
+		return {
+				'name': region['name'],
+				'id': region['id'],
+				'number_countries': 0,
+				'total_population': 0
+		}
 
 def stats(request):
-    # TODO - Provide name, number_countries and total_population for each region
-    response = {"regions": []}
+		countries = list(Country.objects.values())
+		regions = list(Region.objects.values())
+		org_list = list(map(init_regions, regions))
+		for cou in countries:
+				for reg in org_list:
+						if cou['region_id'] == reg['id']:
+								reg['number_countries'] += 1
+								reg['total_population'] += cou['population']
+		for reg in org_list:
+			reg.pop('id', None)
 
-    return JsonResponse(response)
+		response = {"regions": org_list}
+		return JsonResponse(response)
